11e6feb6426c03b1af1e2d9d152c54d7b246f5dd
dmetehan <m.doyran@uu.nl>
2024-02-28 18:13:22 +0100

    bug in cross_validation parameters is fixed!

--- src/cross_validation.py
+++ src/cross_validation.py
@@ -12,13 +12,14 @@ from src.processor import Processor
 
 
 def random_loss_weights_generator():
-    # this function generates 4 values between 0-1 and they sum up to 1
-    separators = [random.randint(0, 10) for _ in range(3)]  # this divides 0-1 region into 0.1 steps with 3 separators
-    separators = sorted(separators)
-    yield {'42': 0.1 * separators[0],
-           '12': 0.1 * (separators[1] - separators[0]),
-           '21x21': 0.1 * (separators[2] - separators[1]),
-           '6x6': 0.1 * (10 - separators[2])}
+    for _ in range(100):
+        # this function generates 4 values between 0-1 and they sum up to 1
+        separators = [random.randint(0, 10) for _ in range(3)]  # this divides 0-1 region into 0.1 steps with 3 separators
+        separators = sorted(separators)
+        yield {'42': 0.1 * separators[0],
+               '12': 0.1 * (separators[1] - separators[0]),
+               '21x21': 0.1 * (separators[2] - separators[1]),
+               '6x6': 0.1 * (10 - separators[2])}
 
 
 def decided_weights_generator():
@@ -62,8 +63,8 @@ def main():
     sleep(args.delay_hours * 3600)
     paramset = set_hyperparameter_generators()
     for key in paramset:
-        for _ in range(100):
-            args.model_args[key] = next(paramset[key]())
+        for model_args in paramset[key]():
+            args.model_args[key] = model_args
             best_states = run_cross_validation(args)
             with open('cross_val_results.txt', 'a+') as f:
                 f.write(json.dumps([best_states] + args.model_args[key]))
